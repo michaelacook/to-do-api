0abaf033352910a7161cf597dff2e6d068c2b076
Michael A. Cook <mcook0775@gmail.com>
2021-04-21 17:54:21 -0400

    Finish service methods

--- services/category.js
+++ services/category.js
@@ -31,6 +31,12 @@ module.exports = {
     }
   },
 
+  /**
+   * Get a single category with associated Lists and ListItems
+   * @param {Number} id - primary key 
+   * @param {Object|Array} lists 
+   * @returns {Promise} category object
+   */
   async getCategory(id, lists = false) {
     try {
       await Category.sync()
@@ -56,4 +62,44 @@ module.exports = {
       return Promise.reject(err.message)
     }
   },
+
+  /**
+   * Add a new category to the database
+   * @param {Object} destructured payload containing userId and title
+   * @returns {Promise} id - primary key for created category
+   */
+  async addCategory({ userId, title }) {
+    try {
+      await Category.sync()
+      const category = await Category.create({
+        userId, 
+        title
+      })
+      return category.id
+    } catch (err) {
+      return Promise.reject(err)
+    }
+  },
+
+  /**
+   * Hard delete a category in the database 
+   * Because onDelete set to cascade, all associated List and ListItems are deleted as well
+   * @param {Number} id - primary key for category
+   * @returns {Promise} true
+   */
+  async deleteCategory(id) {
+    try {
+      await Category.sync()
+      const category = await Category.findByPk(id) 
+      if (!category) {
+        const err = new Error("Not found")
+        err.status = 404
+        return Promise.reject(err)
+      }
+      await category.destroy()
+      return true
+    } catch (err) {
+      return Promise.reject(err)
+    }
+  }
 }
