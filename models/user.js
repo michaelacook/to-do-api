f008072e2ceda3a561647b795a55c322a52118f2
Michael A. Cook <mcook0775@gmail.com>
2021-04-21 19:40:24 -0400

    Remove unused var

--- models/user.js
+++ models/user.js
@@ -2,7 +2,7 @@
 const { Model } = require("sequelize")
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
-    static associate({ Category, List }) {
+    static associate({ Category }) {
       User.hasMany(Category, {
         foreignKey: "userId",
       })
