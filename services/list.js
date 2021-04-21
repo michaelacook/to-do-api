fd4e2f9974cac6525e66745e366a238e9b36986e
Michael A. Cook <mcook0775@gmail.com>
2021-04-21 18:49:56 -0400

    Finish list service

--- services/list.js
+++ services/list.js
@@ -27,4 +27,66 @@ module.exports = {
       return Promise.reject(err)
     }
   },
+
+  /**
+   * Add a new to-do list to the database
+   * @param {Object} destructured - payload containing categoryId and title
+   * @returns {Promise} id for created list
+   */
+  async addList({ categoryId, title }) {
+    try {
+      await List.sync()
+      const list = await List.create({
+        categoryId,
+        title,
+      })
+      return list.id
+    } catch (err) {
+      return Promise.reject(err)
+    }
+  },
+
+  /**
+   * Update a list's title
+   * @param {Object} destructured - payload containing list id and new title
+   * @returns {Promise} true
+   */
+  async updateListTitle({ id, title }) {
+    try {
+      await List.sync()
+      const list = await List.findByPk(id)
+      if (!list) {
+        const err = new Error("Not found")
+        err.status = 404
+        throw err
+      }
+      list.title = title
+      await list.save()
+      return true
+    } catch (err) {
+      return Promise.reject(err)
+    }
+  },
+
+  /**
+   * Hard delete a to do list in the database
+   * Because of the foreign key constraint on list items, deleting a list deletes all associated items
+   * @param {Number} id - list primary key
+   * @returns {Promise} true
+   */
+  async deleteList(id) {
+    try {
+      await List.sync()
+      const list = await List.findByPk(id)
+      if (!list) {
+        const err = new Error("Not found")
+        err.status = 404
+        throw err
+      }
+      await list.destroy()
+      return true
+    } catch (err) {
+      return Promise.reject(err)
+    }
+  },
 }
