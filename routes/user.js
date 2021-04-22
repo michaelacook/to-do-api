7a7b20e4020683d1ce097d32592fd29d19952026
Michael A. Cook <mcook0775@gmail.com>
2021-04-22 13:45:54 -0400

    Add user PUT route and test

--- routes/user.js
+++ routes/user.js
@@ -31,4 +31,20 @@ router.post("/", validateUserMiddleware, async (req, res, next) => {
   }
 })
 
+router.put(
+  "/:id",
+  authMiddleWare,
+  userExistsMiddleware,
+  async (req, res, next) => {
+    try {
+      const { id } = req.params,
+        { body } = req
+      const user = await userService.updateUser(id, body)
+      return res.status(200).json(user)
+    } catch (err) {
+      next(err)
+    }
+  }
+)
+
 module.exports = router
---
+++ test/integration/lib/user.put.test.js
@@ -0,0 +1,33 @@
+const request = require("supertest")
+const app = require("../../../app")
+const { assert } = require("chai")
+
+module.exports = () => {
+  describe("PUT user routes", () => {
+    it("returns 200 OK and an updated user object on happy path", (done) => {
+      request(app)
+        .put("/1")
+        .auth("mcook0775@gmail.com", process.env.PASSWORD)
+        .send({
+          firstName: "Updated Name",
+        })
+        .set("Accept", "application/json")
+        .expect("Content-Type", /json/)
+        .expect(200)
+        .then((response) => {
+          assert.isObject(response.body)
+          assert.hasAllDeepKeys(response.body, [
+            "email",
+            "password",
+            "firstName",
+            "lastName",
+          ])
+          done()
+        })
+        .catch((err) => {
+          console.log(err)
+          done()
+        })
+    })
+  })
+}
