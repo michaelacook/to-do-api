cc06d281355f857ae866341da1cb3d387f4d1116
Michael A. Cook <mcook0775@gmail.com>
2021-04-21 20:31:18 -0400

    Add global error handler

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express")
 const path = require("path")
 const cookieParser = require("cookie-parser")
 const logger = require("morgan")
+const createError = require("http-errors")
 
 const indexRouter = require("./routes/index")
 const usersRouter = require("./routes/users")
@@ -15,5 +16,20 @@ app.use(cookieParser())
 app.use(express.static(path.join(__dirname, "public")))
 
 app.use("/", indexRouter)
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  next(createError(404))
+})
+
+// error handler
+app.use((err, req, res, next) => {
+  // set locals, only providing error in development
+  res.locals.message = err.message
+  res.locals.error = req.app.get("env") === "development" ? err : {}
+
+  // render the error page
+  res.status(err.status || 500).json(err)
+})
 
 module.exports = app
