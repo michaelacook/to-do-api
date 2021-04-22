3e9dcb8ee7f95da5498118640933e1265dbd7dd7
Michael A. Cook <mcook0775@gmail.com>
2021-04-21 21:34:44 -0400

    Add last user test

--- test/integration/lib/user.test.js
+++ test/integration/lib/user.test.js
@@ -27,6 +27,13 @@ module.exports = () => {
       it("returns 401 Unauthorized when auth credentials not given", (done) => {
         request(app).get("/users/1").expect(401, done)
       })
+
+      it("returns 404 Not Found when non-existent id passed", (done) => {
+        request(app)
+          .get("/users/2")
+          .auth("mcook0775@gmail.com", process.env.PASSWORD)
+          .expect(404, done)
+      })
     })
   })
 }
