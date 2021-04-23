9a9b352bf3205109bd52bd14eeb9ca380c608c66
Michael A. Cook <mcook0775@gmail.com>
2021-04-22 20:23:07 -0400

    Add post route

--- routes/category.js
+++ routes/category.js
@@ -4,6 +4,7 @@ const router = express.Router()
 const categoryService = require("../services/category")
 const authMiddleWare = require("../middleware/authentication")()
 const categoryExistsMiddleWare = require("../middleware/categoryExists")()
+const validateCategoryMiddleware = require("../middleware/validateCategory")()
 
 router.get("/", authMiddleWare, async (req, res, next) => {
   try {
@@ -24,6 +25,21 @@ router.get(
       const { id } = req.params
       const category = await categoryService.getCategory(id)
       return res.status(200).json(category)
+    } catch (err) {
+      next(err)
+    }
+  }
+)
+
+router.post(
+  "/",
+  authMiddleWare,
+  validateCategoryMiddleware,
+  async (req, res, next) => {
+    try {
+      const { body } = req
+      const category = await categoryService.addCategory(body)
+      return res.status(201).json(category)
     } catch (err) {
       next(err)
     }
