0cefbef38820c4e4b612b9ca41c3d317651bca11
Michael A. Cook <mcook0775@gmail.com>
2021-04-25 12:30:08 -0400

    Remove unneeded middleware and comments

--- app.js
+++ app.js
@@ -3,8 +3,6 @@ if (process.env.NODE_ENV !== "production") {
 }
 
 const express = require("express")
-const path = require("path")
-const cookieParser = require("cookie-parser")
 const logger = require("morgan")
 const createError = require("http-errors")
 const cors = require("cors")
@@ -20,8 +18,6 @@ app.use(cors())
 app.use(logger("dev"))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
-app.use(cookieParser())
-app.use(express.static(path.join(__dirname, "public")))
 
 app.use("/users", userRouter)
 app.use("/categories", categoryRouter)
@@ -38,8 +34,6 @@ app.use((err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message
   res.locals.error = req.app.get("env") === "development" ? err : {}
-
-  // render the error page
   res.status(err.status || 500).json(err)
 })
 
