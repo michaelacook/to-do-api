077afc4ee1d1d65fda338679fa87bec085ff5165
Michael A. Cook <mcook0775@gmail.com>
2021-04-25 12:26:50 -0400

    Add listItem routes

---
+++ routes/listItem.js
@@ -0,0 +1,76 @@
+const express = require("express")
+const router = express.Router()
+
+const listItemService = require("../services/listItem")
+const authMiddleware = require("../middleware/authentication")()
+const listItemExistsMiddleware = require("../middleware/listItemExists")()
+const validateListItemMiddleware = require("../middleware/validateListItem")()
+
+router.get(
+  "/:id",
+  authMiddleware,
+  listItemExistsMiddleware,
+  async (req, res, next) => {
+    try {
+      const { id } = req.params
+      const listItem = await listItemService.getListItem(id)
+      return res.status(200).json(listItem)
+    } catch (err) {
+      next(err)
+    }
+  }
+)
+
+router.post(
+  "/",
+  authMiddleware,
+  validateListItemMiddleware,
+  async (req, res, next) => {
+    try {
+      const { body } = req
+      const listItem = await listItemService.createListItem(body)
+      return res.status(201).json(listItem)
+    } catch (err) {
+      next(err)
+    }
+  }
+)
+
+router.put(
+  "/:id",
+  authMiddleware,
+  listItemExistsMiddleware,
+  (req, res, next) => {
+    if (!Object.keys(req.body).length) {
+      return res.status(400).end()
+    }
+    return next()
+  },
+  async (req, res, next) => {
+    try {
+      const { id } = req.params
+      const { body } = req
+      const listItem = await listItemService.updateListItem(id, body)
+      return res.status(200).json(listItem)
+    } catch (err) {
+      next(err)
+    }
+  }
+)
+
+router.delete(
+  "/:id",
+  authMiddleware,
+  listItemExistsMiddleware,
+  async (req, res, next) => {
+    try {
+      const { id } = req.params
+      await listItemService.deleteListItem(id)
+      return res.status(204).end()
+    } catch (err) {
+      next(err)
+    }
+  }
+)
+
+module.exports = router
