6667812bf44051e87ad814e62e8036e0691f3aa3
Michael A. Cook <mcook0775@gmail.com>
2021-04-24 22:14:58 -0400

    Add middleware to validate new list item

---
+++ middleware/validateListItem.js
@@ -0,0 +1,13 @@
+/**
+ * Validate client data to create new listItem 
+ * If data don't contain required properties return 404 Bad Request
+ */
+module.exports = () => {
+  return function (req, res, next) {
+    const { body } = req
+    if (!body.listId || !body.content) {
+      return res.status(400).end()
+    }
+    return next()
+  }
+}
